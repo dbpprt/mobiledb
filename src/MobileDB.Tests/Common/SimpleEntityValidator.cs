d1851975b65ec7992fc6aa4eb97f15a53ce4436c
paritoshmmmec <paritoshmmmec@gmail.com>
2015-01-16 15:19:43 +0530

    MobileDB Tests has been included
    MobileDB Tests has been included

---
+++ src/MobileDB.Tests/Common/SimpleEntityValidator.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using MobileDB.Common.Attributes;
+using MobileDB.Common.Validation;
+using MobileDB.Contracts;
+using MobileDB.Exceptions;
+
+namespace MobileDB.Tests.Common
+{
+    public class SimpleEntityValidator : IEntityValidator
+    {
+        public void ValidateEntity(object entity)
+        {
+            var type = entity.GetType();
+            var properties = type.GetRuntimeProperties();
+
+            foreach (var propertyInfo in properties)
+            {
+                var validationAttributes = propertyInfo.GetCustomAttributes<ValidationAttribute>();
+
+                var context = new EntityValidationContext
+                {
+                    MemberName = propertyInfo.Name,
+                    ObjectInstance = entity,
+                    ObjectType = type
+                };
+
+                foreach (var validationAttribute in validationAttributes)
+                {
+                    var result = validationAttribute.Validate(propertyInfo.GetValue(entity), context);
+
+                    if (!result.Succeeded)
+                    {
+                        throw new EntityValidationException(result.ErrorMessage);
+                    }
+                }
+            }
+        }
+    }
+}
